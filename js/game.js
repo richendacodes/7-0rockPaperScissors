311f14025924fab2b7dcdb1149b57898399058a6
Richenda Gould <Richenda Gould>
2015-12-12 18:46:50 -0500

    Leaderboard in prog, pops up on win

--- js/game.js
+++ js/game.js
@@ -1,5 +1,33 @@
 $( document ).ready(function() {
 
+
+  // $("#myModal").on("show", function() {    // wire up the OK button to dismiss the modal when shown
+  //       $("#myModal a.btn").on("click", function(e) {
+  //           console.log("button pressed");   // just as an example...
+  //           $("#myModal").modal('hide');     // dismiss the dialog
+  //       });
+  //   });
+  //   $("#myModal").on("hide", function() {    // remove the event listeners when the dialog is dismissed
+  //       $("#myModal a.btn").off("click");
+  //   });
+    
+  //   $("#myModal").on("hidden", function() {  // remove the actual elements from the DOM when fully hidden
+  //       $("#myModal").remove();
+  //   });
+    
+  //   $("#myModal").modal({                    // wire up the actual modal functionality and show the dialog
+  //     "backdrop"  : "static",
+  //     "keyboard"  : true,
+  //     "show"      : true                     // ensure the modal is shown immediately
+  //   });
+
+
+
+
+
+
+
+
   var playGame = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
   var userScore = 0;
   var computerScore = 0;
@@ -268,23 +296,24 @@ $( document ).ready(function() {
 
   function scoreBoard() {
     if (rounds > 4) {
+      $('.btn-primary').off();
       if (userScore > computerScore) {
         setTimeout(function() {
           $('#msg').empty();
-          bootbox.alert('You win!');
-          $('#rounds').html('You win! Play again?')
+          $('#myModal').modal('show');
+          $('#rounds').html('You win! Play again?');
           $('.btn-primary').addClass('btn-hide slideDown');
           $('.btn-success').removeClass('btn-hide');
           rounds = 0;
           computerScore = 0;
           userScore = 0;
-        }, 1500);
+        }, 1000);
 
       } else if (computerScore > userScore) {
         setTimeout(function() {
           $('#msg').empty();
           bootbox.alert('You lose!');
-          $('#rounds').html('You lose! Play again?')
+          $('#rounds').html('You lose! Play again?');
           $('.btn-primary').addClass('btn-hide slideDown');
           $('.btn-success').removeClass('btn-hide');
           rounds = 0;
@@ -295,7 +324,7 @@ $( document ).ready(function() {
         setTimeout(function() {
           $('#msg').empty();
           bootbox.alert('It\'s a tie!');
-          $('#rounds').html('It\'s a tie!')
+          $('#rounds').html('It\'s a tie!');
           $('.btn-primary').addClass('btn-hide slideDown');
           $('.btn-success').removeClass('btn-hide');
           rounds = 0;
@@ -309,5 +338,82 @@ $( document ).ready(function() {
 
   theGame();
   
+  // Leaderboard ============================================================================================================
+
+  var LEADERBOARD_SIZE = 5;
+
+  // Create our Firebase reference
+  var scoreListRef = new Firebase('https://sizzling-heat-1941.firebaseio.com');
+
+  // Keep a mapping of firebase locations to HTML elements, so we can move / remove elements as necessary.
+  var htmlForPath = {};
+
+  // Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+  function handleScoreAdded(scoreSnapshot, prevScoreName) {
+    var newScoreRow = $("<tr/>");
+    newScoreRow.append($("<td/>").append($("<em/>").text(scoreSnapshot.val().name)));
+    newScoreRow.append($("<td/>").text(scoreSnapshot.val().score));
+
+    // Store a reference to the table row so we can get it again later.
+    htmlForPath[scoreSnapshot.key()] = newScoreRow;
+
+    // Insert the new score in the appropriate place in the table.
+    if (prevScoreName === null) {
+      $("#leaderboardTable").append(newScoreRow);
+    }
+    else {
+      var lowerScoreRow = htmlForPath[prevScoreName];
+      lowerScoreRow.before(newScoreRow);
+    }
+  }
+
+  // Helper function to handle a score object being removed; just removes the corresponding table row.
+  function handleScoreRemoved(scoreSnapshot) {
+    var removedScoreRow = htmlForPath[scoreSnapshot.key()];
+    removedScoreRow.remove();
+    delete htmlForPath[scoreSnapshot.key()];
+  }
+
+  // Create a view to only receive callbacks for the last LEADERBOARD_SIZE scores
+  var scoreListView = scoreListRef.limitToLast(LEADERBOARD_SIZE);
+
+  // Add a callback to handle when a new score is added.
+  scoreListView.on('child_added', function (newScoreSnapshot, prevScoreName) {
+    handleScoreAdded(newScoreSnapshot, prevScoreName);
+  });
+
+  // Add a callback to handle when a score is removed
+  scoreListView.on('child_removed', function (oldScoreSnapshot) {
+    handleScoreRemoved(oldScoreSnapshot);
+  });
+
+  // Add a callback to handle when a score changes or moves positions.
+  var changedCallback = function (scoreSnapshot, prevScoreName) {
+    handleScoreRemoved(scoreSnapshot);
+    handleScoreAdded(scoreSnapshot, prevScoreName);
+  };
+  scoreListView.on('child_moved', changedCallback);
+  scoreListView.on('child_changed', changedCallback);
+
+  // When the user presses enter on scoreInput, add the score, and update the highest score.
+  $("#scoreInput").keypress(function (e) {
+    if (e.keyCode == 13) {
+      var newScore = Number($("#scoreInput").val());
+      var name = $("#nameInput").val();
+      $("#scoreInput").val("");
+
+      if (name.length === 0)
+        return;
+
+      var userScoreRef = scoreListRef.child(name);
+
+      // Use setWithPriority to put the name / score in Firebase, and set the priority to be the score.
+      userScoreRef.setWithPriority({ name:name, score:newScore }, newScore);
+    }
+  });
+
+
+
+
 
 });
